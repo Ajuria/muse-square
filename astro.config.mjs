b55b5e0aeb133e2693b6c5d1c03479add144cdfc
Ajuria <julen.deajuriaguerra@gmail.com>
2025-08-26 17:27:06 +0200

    chore: set site to https://muse-square.vercel.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://musesquare.vercel.app', // tu ajusteras après déploiement
+  site: 'https://muse-square.vercel.app', // tu ajusteras après déploiement
   integrations: [tailwind(), mdx()],
 });
 
