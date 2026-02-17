dd7721b68f5d2d9cd79d9643bb3e9dc5f1b574c6
Ajuria <julen.deajuriaguerra@gmail.com>
2026-02-17 11:01:41 +0100

    chore: set canonical site to musesquare.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,18 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import clerk from "@clerk/astro";
 import { frFR } from "@clerk/localizations";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   site: import.meta.env.PROD
-    ? "https://muse-square.vercel.app"
+    ? "https://musesquare.com"
     : "http://localhost:4322",
 
+  server: { host: true },
+  
   devToolbar: { enabled: false },
-
   output: "server",
-  adapter: node({ mode: "standalone" }),
+  adapter: vercel(),
 
   integrations: [
     tailwind(),
